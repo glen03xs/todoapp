5ad70626311cd57341de3b0ce34b0ea796d14687
glen03xs <glen03xs@gmail.com>
2017-08-29 00:15:50 +0800

    Done Creating Basic Function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-var todos = ["item 1", "item 2", "item 3"];
+
+
+var todos = ['Item 1', 'Item 2', 'Item 3'];
+
+function displayTodo() {
+    console.log('My todos:', todos);
+}
+
+function addTodo(todo) {
+    todos.push(todo);
+    displayTodo();
+}
+
+function editTodo(position, item) {
+    todos[position] = item;
+    displayTodo();
+}
+
+function deleteTodo(position) {
+    todos.splice(position, 1);
+    displayTodo();
+}
